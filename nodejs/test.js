3ef4b70b8c4ea0a01e1b2071dd79fb9db73472da
thtTNT <848815969@qq.com>
2021-03-15 01:24:39 +0800

    添加了NodeJs版本的README.MD

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,9 +1,9 @@
 let pc = require("potato-communicate")
 
 let server = new pc.Server(8080)
-// server.on("connection", (client) => {
-//     client.write({a:"b"})
-// })
+server.on("connection", (client) => {
+    client.write({a:"b"})
+})
 
 let client = pc.Client.connect("127.0.0.1", 2121)
 client.on("data",(data) => console.log(data))
